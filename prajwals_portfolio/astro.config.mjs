deeecde1df9f420b266143955ba05faaddba9d95
Prajwal <prajwaldhule36@gmail.com>
2025-05-02 02:24:08 +0530

    main: completed first (dithering) blog, added giscus comments section & google analytics and polished the UI.

--- prajwals_portfolio/astro.config.mjs
+++ prajwals_portfolio/astro.config.mjs
@@ -7,5 +7,8 @@ import sitemap from '@astrojs/sitemap';
 export default defineConfig({
 	site: 'https://example.com',
 	integrations: [mdx(), sitemap()],
-	redirects: { "/": "/blog" }
+	redirects: { "/": "/blog" },
+	devToolbar: {
+		enabled: false,
+	}
 });
